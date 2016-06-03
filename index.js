9a0d73c292eab6533742dff706413fe7eec290e6
yinyong02 <yinyong02@baidu.com>
2016-06-04 03:24:28 +0800

    local file

--- index.js
+++ index.js
@@ -13,13 +13,11 @@ module.exports = {
     book: {
         assets: "./book",
         js: [
+            "mathjsx.js"
         ],
         css: [
         ],
         html: {
-            "head:end": function() {
-                return "<script async src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'></script>";
-            }
         }
     }
 };
